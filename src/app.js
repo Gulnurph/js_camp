ff1c59180b236f449ff3c9014e2db68d4f284e86
Gulnurph <glnr28@hotmail.com>
2021-10-20 11:30:17 +0300

    20 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("merhaba kodlama.io")
+
+//var dolarDun=9.20
+let dolarBugun=9.30
+//dolarDun="Ankara"
+let dolarDun=9.20
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+const euroDun=11.2
+//euroDun=11//Bunu yapamıyoruz const ile tanımladığımız için
+
+//array
+let konutKredileri=["konut kredisi","Emlak konut kredisi","Kamu konut kredisi"]
+console.log(konutKredileri)
+
+    
+
